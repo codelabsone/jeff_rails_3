742832159ae4829a1c2ff42ebb403f2d486ee158
Jay <JayHG@gmail.com>
2019-09-09 19:21:23 -0500

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190909233037) do
+ActiveRecord::Schema.define(version: 20190909230152) do
 
   create_table "departments", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
