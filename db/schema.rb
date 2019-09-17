ac5bad0f94a7e7b7b05594660147353b6c6cde9d
Jay <JayHG@gmail.com>
2019-09-16 19:03:17 -0500

    added columns to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190909230152) do
+ActiveRecord::Schema.define(version: 20190916235436) do
 
   create_table "departments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "description"
   end
 
 end
