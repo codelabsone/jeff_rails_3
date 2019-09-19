17cc6e3d2bc96561b52f7a16c72e40768e9c537f
user.name <dlegrand1422@gmail.com>
2019-09-19 17:32:41 -0500

    OCD in controller. just cleaned up the spacing

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,8 +2,8 @@ class DepartmentsController < ApplicationController
   def index
     @departments = Department.all
   end
+
   def show
     @departments = Department.show(params[:id])
-
   end
 end
