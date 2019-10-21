7bc1a18aa2b2f828692ce0bcdea3ce152c50a9bc
Jay <JayHG@gmail.com>
2019-10-21 17:42:13 -0500

    Added Home link on Finance page

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -8,9 +8,6 @@ class DepartmentsController < ApplicationController
   end
 
   def create
-    @loan_amount = :loan_amount.to_f
-    @trade_in = :trade_in.to_f
-    @total_loan = @loan_amount - @trade_in
-    redirect_to :back
+
   end
 end
